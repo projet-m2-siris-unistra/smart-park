25c44a9c224c8fce7c94a904c2fc19c55a2930ad
lioneljung <lionel.jung@etu.unistra.fr>
2019-10-21 15:31:39 +0200

    Parking zone menu - problem to fix: static folder on /parking

--- web/hello.py
+++ web/hello.py
@@ -19,9 +19,15 @@ dashboard_template = template_env.get_template("dashboard_template.html")
 zones_template = template_env.get_template("zones_template.html")
 login_template = template_env.get_template("login_template.html")
 signup_template = template_env.get_template("signup_template.html")
+# view of a parking zone, with the listing of all places
+parking_template = template_env.get_template("parking_template.html")
+
 
 app.static("/static", "./static")
 
+
+
+# Handling navigation
 @app.route('/')
 @app.route('/home')
 async def welcome(request):
@@ -39,6 +45,9 @@ async def zones(request):
     rendered_template = await zones_template.render_async()
     return response.html(rendered_template)
 
+
+
+# Handling accounts
 @app.route('/connexion')
 async def login(request):
     rendered_template = await login_template.render_async()
@@ -46,29 +55,60 @@ async def login(request):
 
 @app.route('/login-check', methods=["POST"])
 async def login_check(request):
-        return json({
-        "parsed": True,
-        "url": request.url,
-        "query_string": request.query_string,
-        "args": request.args,
-        "raw_args": request.raw_args,
-        "query_args": request.query_args,
-    })
+    return json({
+    "parsed": True,
+    "url": request.url,
+    "query_string": request.query_string,
+    "args": request.args,
+    "raw_args": request.raw_args,
+    "query_args": request.query_args})
 
 @app.route('/signup-check', methods=["POST"])
 async def login_check(request):
-        return json({
-        "parsed": True,
-        "url": request.url,
-        "query_string": request.query_string,
-        "args": request.args,
-        "raw_args": request.raw_args,
-        "query_args": request.query_args,
-    })
+    return json({
+    "parsed": True,
+    "url": request.url,
+    "query_string": request.query_string,
+    "args": request.args,
+    "raw_args": request.raw_args,
+    "query_args": request.query_args})
 
 @app.route('/signup')
 async def login(request):
     rendered_template = await signup_template.render_async()
     return response.html(rendered_template)
 
+
+
+
+# Handling Parkings zones
+@app.route('/parking/<zone>')
+@app.route('/parking/<zone>/overview')
+async def parking_view(request, zone):
+    rendered_template = await parking_template.render_async(
+        active_tab_view='true', zone=zone)
+    return response.html(rendered_template)
+
+@app.route('/parking/<zone>/spots')
+async def parking_spots(request, zone):
+    rendered_template = await parking_template.render_async(
+        active_tab_list='true', zone=zone)
+    return response.html(rendered_template)
+
+@app.route('/parking/<zone>/statistics')
+async def parking_spots(request, zone):
+    rendered_template = await parking_template.render_async(
+        active_tab_stats='true', zone=zone)
+    return response.html(rendered_template)
+
+@app.route('/parking/<zone>/configuration')
+async def parking_spots(request, zone):
+    rendered_template = await parking_template.render_async(
+        active_tab_config='true', zone=zone)
+    return response.html(rendered_template)
+
+
+
+
+# starting website
 app.run(host="0.0.0.0", port=8080, debug=True)
