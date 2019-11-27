cdd149ca4c43fbb583668a90325388cc19c5e64e
DIVRIOTIS Constantin <constantin.divriotis@etu.unistra.fr>
2019-11-27 16:39:07 +0100

    backend : places.list depending on zone_id / modify devices.update

--- backend/handlers/places.go
+++ backend/handlers/places.go
@@ -11,14 +11,18 @@ type getPlaceRequest struct {
 	PlaceID int `json:"place_id"`
 }
 
+type getPlacesRequest struct {
+	ZoneID int `json:"zone_id"`
+}
+
 func getPlace(ctx context.Context, request getPlaceRequest) (database.Place, error) {
 	log.Println("handlers: handling getPlace")
 
 	return database.GetPlace(ctx, request.PlaceID)
 }
 
-func getPlaces(ctx context.Context, request getPlaceRequest) ([]database.Place, error) {
+func getPlaces(ctx context.Context, request getPlacesRequest) ([]database.Place, error) {
 	log.Println("handlers: handling getPlaces")
 
-	return database.GetPlaces(ctx)
+	return database.GetPlaces(ctx, request.ZoneID)
 }
