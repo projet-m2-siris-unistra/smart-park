2d7ba7ad1cd8562f658c20c99efcb9ef89ff49e0
Quentin Gliech <quentingliech@gmail.com>
2019-11-30 20:17:03 +0100

    navigator: map view with bottom sheet

--- navigator/app/src/main/java/fr/smartpark/navigator/HomeFragment.kt
+++ navigator/app/src/main/java/fr/smartpark/navigator/HomeFragment.kt
@@ -1,23 +1,34 @@
 package fr.smartpark.navigator
 
 
+import android.R.attr.left
+import android.R.attr.right
+import android.animation.AnimatorInflater
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import fr.smartpark.navigator.databinding.FragmentHomeBinding
+
 
 /**
  * The Home [Fragment] that displays a map and a list of zones.
  */
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val bottomSheetBehavior = BottomSheetBehavior.from(binding.bottomSheet)
+        bottomSheetBehavior.apply {
+            saveFlags = BottomSheetBehavior.SAVE_ALL
+        }
+        return binding.root
     }
 
 
