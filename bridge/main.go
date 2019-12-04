23f24c3af40bd02330d0345b417c2b00e6e425da
info <info@reso17.u-strasbg.fr>
2019-12-04 16:44:37 +0100

    Recuperation infos et envoi sur bus

--- bridge/main.go
+++ bridge/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -8,9 +10,64 @@ import (
 	"github.com/nats-io/nats.go"
 )
 
+type deviceInfos struct {
+	DeviceName      string `json:"deviceName"`
+	DevEUI          string `json:"devEUI"`
+	ApplicationName string `json:"applicationName"`
+}
+
+type outputInfos struct {
+	Device_EUI		string `json:"device_EUI"`
+	State			string `json:"state"`
+	Battery			int    `json:"battery"`
+}
+
+type listRequest struct{}
+
 var nc *nats.Conn
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+
+	var u deviceInfos
+
+	//	err2 := json.NewDecoder(r.Body).Decode(&u)
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	err2 := json.Unmarshal(body, &u)
+
+	if err2 != nil {
+		http.Error(w, err2.Error(), 400)
+		return
+	}
+
+	log.Print(u)
+
+	var out outputInfos
+
+	out.Device_EUI = u.DevEUI
+	out.State = "occupied"
+	out.Battery = 43
+
+	requestBody, err3 := json.Marshal(out)
+
+	if err3 != nil {
+		log.Fatalln(err3)
+	}
+
+	log.Printf("sending update %v", out)
+	err = nc.Publish("devices.update", requestBody)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//w.Write([]byte("DeviceName : %s, DevEUI : %s, Battery : %s\n",u.deviceName, u.devEUI, u.applicationName))
+
+
 	w.Write([]byte("ok"))
 }
 
