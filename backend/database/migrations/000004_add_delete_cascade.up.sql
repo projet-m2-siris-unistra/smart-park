b1b26d7ee5278be7772cfd50cf06da6b4f99d938
Quentin Gliech <quentingliech@gmail.com>
2020-01-05 18:34:02 +0100

    database: add a few cascades

---
+++ backend/database/migrations/000004_add_delete_cascade.up.sql
@@ -0,0 +1,20 @@
+-- PLACES (fk : place_id)
+ALTER TABLE places
+DROP CONSTRAINT places_device_id_fkey;
+
+ALTER TABLE places
+ADD CONSTRAINT places_device_id_fkey
+FOREIGN KEY (device_id)
+REFERENCES devices (device_id)
+ON DELETE SET NULL;
+
+
+-- DEVICES (fk : tenant_id)
+ALTER TABLE devices
+DROP CONSTRAINT devices_tenant_id_fkey;
+
+ALTER TABLE devices
+ADD CONSTRAINT devices_tenant_id_fkey
+FOREIGN KEY (tenant_id)
+REFERENCES tenants (tenant_id)
+ON DELETE CASCADE;
