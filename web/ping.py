febc39d6e5bedd5ecc63034cb0e28578ff1b92b1
Quentin Gliech <quentingliech@gmail.com>
2020-01-13 14:40:17 +0100

    utils: decode json

--- web/ping.py
+++ web/ping.py
@@ -1,3 +1,4 @@
+import json
 import asyncio
 import sys
 
@@ -13,7 +14,8 @@ async def run():
         payload = bytes(sys.argv[2], "utf-8")
     try:
         response = await nc.request(sys.argv[1], payload, timeout=1)
-        print(response.data)
+        data = json.loads(response.data)
+        print(json.dumps(data, indent=2))
     except ErrTimeout:
         print("Timeout atteint : stop process")
 
